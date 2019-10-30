1e5206bcfb8fe6a7c0709eaada50fa6561e22bc2
Jitesh <jiteshvm@gmail.com>
2019-10-30 00:59:13 -0700

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,27 @@
 using namespace std;
 const char* filename = "outputimg.ppm";
 
+vec3 white(1.0f, 1.0f, 1.0f);
+vec3 blue(0.5f, 0.7f, 1.0f);
+vec3 green(0.0f, 1.0f, 0.0f);
+
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+	vec3 oc = r.origin() - center;
+	float a = dot(r.direction(), r.direction());
+	float b = 2 * dot(oc, r.direction());
+	float c = dot(oc, oc) - radius * radius;
+	float discriminant = b * b - 4 * a * c;
+	return (discriminant > 0);
+}
+
 vec3 color(const ray& r)
 {
+	if (hit_sphere(vec3(0, 0, -1), 0.5f, r))
+		return green;                                                                                  
 	vec3 unit_direction = unit_vector(r.direction());
 	float t = 0.5f * (unit_direction.y() + 1.0f);
-	return (1.0f - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+	return (1.0f - t) * white + t * blue;
 }
 
 int main()
@@ -33,7 +49,9 @@ int main()
 		vec3 horizontal(4.0f, 0.0f, 0.0f);
 		vec3 vertical(0.0f, 2.0f, 0.0f);
 		vec3 origin(0.0f, 0.0f, 0.0f);
-		vec3 dir;
+		vec3 dir(0.0f, 0.0f, 1.0f);
+		ray t(origin, dir);
+
 		for (int j = ny - 1; j >= 0; --j) {
 			for (int i = 0; i < nx; ++i) {
 				float u = float(i) / float(nx);
@@ -41,17 +59,18 @@ int main()
 				dir = lower_left_corner + u*horizontal + v*vertical;
 				ray r(origin, dir);
 				vec3 col = color(r);
-				int ir = int(255.99 * col[0]);
-				int ig = int(255.99 * col[1]);
-				int ib = int(255.99 * col[2]);
+				int ir = int(255.0 *col[0]);
+				int ig = int(255.0 * col[1]);
+				int ib = int(255.0 * col[2]);
+
 				//cout << ir << " " << ig << " " << ib << "\n";
 				outstream << ir << " " << ig << " " << ib << "\n";
 			}
 		}
 	}
 
-	//outstream.close();
-	cout << "done";
-	cin.get();
+	outstream.close();
+	//cout << "done";
+	//cin.get();
 	return 0;
 }
--- src/ray.h
+++ src/ray.h
@@ -10,7 +10,7 @@ public:
 	ray(const vec3& origin, const vec3& dir) { A = origin; B = dir; }
 	vec3 origin() const { return A; }
 	vec3 direction() const { return B; }
-	vec3 point_at_parameter(float t) const { A + t*B; }
+	vec3 point_at_parameter(float t) const { return A + t*B; }
 
 	vec3 A;
 	vec3 B;
